7afe60d0b55672fb533397b4f7c44455aa822876
nevenjovanovic <neven.jovanovic@ffzg.hr>
2019-10-04 21:03:10 +0200

    Updates

---
+++ precache-manifest.675aff7a86ff30b4641fe59b2d32de6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd8be05eb23ec636367863a87c1974e",
+    "url": "/grp-grc-treebanks/index.html"
+  },
+  {
+    "revision": "f4357963f30dc5752df4",
+    "url": "/grp-grc-treebanks/static/css/2.be8e0034.chunk.css"
+  },
+  {
+    "revision": "87bc2f74c84bb9582d10",
+    "url": "/grp-grc-treebanks/static/css/main.a730fb68.chunk.css"
+  },
+  {
+    "revision": "f4357963f30dc5752df4",
+    "url": "/grp-grc-treebanks/static/js/2.a8069619.chunk.js"
+  },
+  {
+    "revision": "87bc2f74c84bb9582d10",
+    "url": "/grp-grc-treebanks/static/js/main.b52c9a93.chunk.js"
+  },
+  {
+    "revision": "221e9da21b03c05e1d7a",
+    "url": "/grp-grc-treebanks/static/js/runtime~main.564e8ab3.js"
+  }
+]);
